82f3c6aa6e3a53ef0cb625a4f5a2846ebfcfd61d
Rodrigo Herrera <rherrera@verticallsoft.cl>
2019-07-11 11:41:35 -0400

    -Servicio SOAP y REST utilizando JPA y portlet con SearchContainer

--- list-discos-portlet/src/main/java/cl/az/test/helper/Constants.java
+++ list-discos-portlet/src/main/java/cl/az/test/helper/Constants.java
@@ -3,5 +3,10 @@ package cl.az.test.helper;
 public class Constants {
     public static String HOST = "localhost";
     public static int PORT = 8080;
+
+    // Endpoint para ejecucion en Tomcat
     public static String LIST_SERVICE_ENDPOINT="/service-1.0-SNAPSHOT/rest/DiscoService/list/";
+
+    // Endpoint para uso en ejecucion como aplicacion independiente de componente service
+    //public static String LIST_SERVICE_ENDPOINT="/DiscoService/list/";
 }
